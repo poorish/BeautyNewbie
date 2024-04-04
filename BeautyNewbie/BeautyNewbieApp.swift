a0ef40e77cbca40e6eaae4cda9a016ab610dfecc
pooh <poorish91@gmail.com>
2024-04-04 23:10:47 +0700

    Initial Commit

---
+++ BeautyNewbie/BeautyNewbieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyNewbieApp.swift
+//  BeautyNewbie
+//
+//  Created by Poorish Charoenkul on 4/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct BeautyNewbieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeautyNewbie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeautyNewbie
+//
+//  Created by Poorish Charoenkul on 4/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
